3ea29befa838380611e12e2a679f67b616714526
Suyash <suyash@Suyashs-MacBook-Pro.local>
2018-12-06 14:01:38 +0530

    Intial commit

---
+++ app/src/main/java/com/easy/pg/app/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.easy.pg.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
